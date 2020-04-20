951b2ad4e820965e13ad33ac76343944a659458e
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-20 21:47:28 +0530

    :rainbow: golang switch statement exercise

---
+++ 03_basic_syntax/switch.go
@@ -0,0 +1,17 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var str string
+// 	switch str {
+// 	case "hello":
+// 		fmt.Println("This is hello")
+// 	case "world":
+// 		fmt.Println("This is world")
+// 	case "lets", "go":
+// 		fmt.Println("Lets GOOOO!!!")
+// 	default:
+// 		fmt.Println("Default case...")
+// 	}
+// }
