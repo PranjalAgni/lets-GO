22c8588c8cf2bd4ae1cd6511a3ea5264d939dabf
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-20 21:48:04 +0530

    :rainbow: golang how to define variables

---
+++ 03_basic_syntax/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var someNumber int = 2
+	var someName string = "Beyonce"
+	fmt.Println(someNumber, someName)
+	if someNumber := 3; someNumber < 12 {
+		fmt.Println("This is greater:", someNumber)
+	}
+}
+
+
+// irox
+// cross func
+// login route
+// collection
+// agent cms
+// cls gateway
+// cls common
+// cls model
