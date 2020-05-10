7e9ca839d0afcb92a74447ea12ac267bd96668eb
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-05-10 22:18:05 +0530

    :sparkles: playing with arrays in go

---
+++ 04_complex_structures/arrays.go
@@ -0,0 +1,10 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var weights [5]float64 = [5]float64{41.5, 50.5, 55.8, 60.2, 65.5}
+// 	fruits := [5]string{"banana", "apple", "pappya", "kiwi", "strawberry"}
+// 	fmt.Println("Length of wow", len(weights))
+// 	fmt.Println("Length of fruits", len(fruits))
+// }
